fcc33f6ba5cb45442532fffadb3ea412e31e5133
santhosh kumar <santhoshssk070403@gmail.com>
2025-10-08 16:27:19 +0530

    second commit

---
+++ mark.py
@@ -0,0 +1,11 @@
+tamil=int(input("enter you tamil mark:"))
+english=int(input("enter your english mark:"))
+maths=int(input("enter your maths mark:"))
+science=int(input("enter your science mark:"))
+social=int(input("enter your social mark:"))
+total=tamil+english+maths+science+social
+pointtotal=total\500*100
+print(tamil,english,maths,science,social)
+print(total)
+print(pointtotal)
+           
