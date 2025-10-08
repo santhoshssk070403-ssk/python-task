49d6c521fd034ec8609f50c9b5c35663cf154c91
santhosh kumar <santhoshssk070403@gmail.com>
2025-10-08 16:29:20 +0530

    third commit

---
+++ mark2.py
@@ -0,0 +1,10 @@
+tamil=int(input("enter you tamil mark:"))
+english=int(input("enter your english mark:"))
+maths=int(input("enter your maths mark:"))
+computer=int(input("enter your CS mark:"))
+chem=int(input("enter your CHEM mark:"))
+phy=int(input("enter your PHY mark:"))
+total=tamil+english+maths+computer+chem+phy
+percentage=total/600*100
+print(total)
+print(percentage)
