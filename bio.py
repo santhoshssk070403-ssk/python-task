8f72a97b526e799ca6f718e80e277681c29d168e
santhosh kumar <santhoshssk070403@gmail.com>
2025-10-08 16:34:10 +0530

    fifth commit

---
+++ bio.py
@@ -0,0 +1,9 @@
+print("name:santhosh kumar")
+print("place:villapuram")
+print("age:22")
+print("edu:MSC computer science")
+print("work exp:cashier")
+print("proj:social midea traker")
+print("hobbies:movies,animes")
+
+
