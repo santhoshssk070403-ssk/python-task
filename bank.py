cd3fd6d2fe6d080792e8bcd9a55bdfa26dada3b6
santhosh kumar <santhoshssk070403@gmail.com>
2025-10-08 16:15:21 +0530

    first commit

---
+++ bank.py
@@ -0,0 +1,20 @@
+name=str(input("enter you name:"))
+accono=int(input("enter your account:"))
+ifsc=int(input("enter your ifsc :"))
+email=str(input("enter your email:"))
+phno=int(input("enter your phone no:"))
+bankbal=float(input("enter your bank balance:"))
+'\n'
+print( "--------------Bank detail--------------" )
+'\n'
+
+
+
+print("Name;",name)
+print("Account no:",accono)
+print("IFSC:",ifsc)
+print("Email:",email)
+print("Phone no:",phno)
+print("Bank Balance:",bankbal)
+
+
